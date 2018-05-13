fa36ca4768a92f34db69a2f2dc2effacd6380c33
PJ Matteucci <pmatteu2@gmail.com>
2018-05-13 00:17:29 -0500

    starting

---
+++ client/index.js
@@ -0,0 +1,10 @@
+//
+// The main client file.
+// Replace this using the pult command. For example:
+//
+//    $ pult add mithril
+//
+// See all options here: https://github.com/Concatapult/pult
+//
+
+document.querySelector('h1').innerHTML += ' (SPA style!)'
